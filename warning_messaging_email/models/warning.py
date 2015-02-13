d8291a6fb51920d8835512b5dfc8c381a86f039f
Azucena Luque <azucena@trey.es>
2015-02-13 11:51:39 +0100

    [IMP] Funcionalidad de envío de correos llevada a la acción del aviso, que es donde tiene que estar + modificaciones aplicadas a los demás módulos.

--- warning_messaging_email/models/warning.py
+++ warning_messaging_email/models/warning.py
@@ -27,29 +27,10 @@ _log = logging.getLogger(__name__)
 class WarningMessaging(models.Model):
     _inherit = 'warning.messaging'
 
-    email_tmpl_id = fields.Many2one(
-        comodel_name='email.template',
-        string='Email Template')
-    email_type = fields.Selection(
-        selection=[
-            ('with_template', 'With template'),
-            ('without_template', 'Without template'),
-        ],
-        string='Email type',
-        default='with_template',
-        required=True)
-    email_subject = fields.Char(string='Email subject')
-    email_body_html = fields.Char(string='Email body html')
-    email_attachment_ids = fields.Many2many(
-        comodel_name='ir.attachment',
-        relation='warning_mess_attach_rel',
-        column1='warning_mess_id',
-        column2='attachment_id')
-
     @api.multi
-    def send_mail_without_template(self, email_to):
+    def send_mail_without_template(self, email_to, action):
         ''' Send a mail without template to email address indicated in
-        'email_to'.'''
+        'email_to' with action values.'''
         cr, uid, context = self.env.args
 
         # 1 - Crear objeto mail.message (mail_message_id)
@@ -60,11 +41,11 @@ class WarningMessaging(models.Model):
                 'Not exist any mail in active state, the email can not sent.'))
 
         data_msg = {
-            'subject': self.email_subject or '',
+            'subject': action.email_subject or '',
             'type': 'email',
             'email_from': email_from.smtp_user,
             'attachment_ids': [(6, 0, [attach.id for attach in
-                                self.email_attachment_ids])]
+                                action.email_attachment_ids])]
         }
         mail_message = self.env['mail.message'].create(data_msg)
 
@@ -72,7 +53,7 @@ class WarningMessaging(models.Model):
         mail_mail = self.env['mail.mail']
         data_mail = {
             'mail_message_id': mail_message.id,
-            'body_html': self.email_body_html or '',
+            'body_html': action.email_body_html or '',
             'email_to': email_to
         }
         mail = mail_mail.create(data_mail)
@@ -84,51 +65,57 @@ class WarningMessaging(models.Model):
         return True
 
     @api.one
-    def do_send_email(self, objs):
+    def do_send_email_without_templ(self, objs, action):
+        ''' Send a email without template with action values.'''
         try:
             for obj in objs:
                 if hasattr(obj, 'message_post'):
-                    # Si el aviso no tiene plantilla asignada
-                    if not self.email_tmpl_id.exists():
-                        email_to = obj.partner_id and obj.partner_id.email\
-                            or None
-                        self.send_mail_without_template(email_to)
+                    email_to = obj.partner_id and obj.partner_id.email\
+                        or None
+                    # Enviar correo
+                    self.send_mail_without_template(email_to, action)
 
-                        # Notificar en el registro que se ha enviado el
-                        # correo
-                        partner_ids = [obj.user_id and obj.user_id.partner_id
-                                       and obj.user_id.partner_id.id] or []
-                        body = _('Mail sent to partner from warning \'%s\'.'
-                                 % self.name)
-                        obj.with_context(
-                            mail_post_autofollow=False).message_post(
-                            body=body, partner_ids=partner_ids)
+                    # Notificar en el registro que se ha enviado el
+                    # correo
+                    partner_ids = [obj.user_id and obj.user_id.partner_id
+                                   and obj.user_id.partner_id.id] or []
+                    body = _('Mail sent to partner from warning \'%s\'.'
+                             % self.name)
+                    obj.with_context(
+                        mail_post_autofollow=False).message_post(
+                        body=body, partner_ids=partner_ids)
+                else:
+                    _log.error('%s model don\'t inherit mail.message, '
+                               'not message sended.' % self.model_id.model)
+            return True
+        except Exception as e:
+            _log.error('I can\'t to send the message for warning "%s": %s'
+                       % (self.name, e))
+            return False
 
-                    # Si el aviso tiene plantilla asignada
-                    else:
-                        # Enviar directamente con la plantilla seleccionada
-                        # por el usuario en el aviso
+    @api.one
+    def do_send_email_with_templ(self, objs, action):
+        ''' Send a email with template defined in action.'''
+        try:
+            for obj in objs:
+                if hasattr(obj, 'message_post'):
+                    # Para que funcione el envio de correos, la llamada
+                    # debe ser con la antigua api
+                    cr, uid, context = self.env.args
+                    self.pool.get('email.template').send_mail(
+                        cr, uid, action.email_tmpl_id.id, obj.id,
+                        force_send=True, raise_exception=True,
+                        context=context)
 
-                        # Para que funcione el envio de correos, la llamada
-                        # debe ser con la antigua api
-                        cr, uid, context = self.env.args
-                        self.pool.get('email.template').send_mail(
-                            cr, uid, self.email_tmpl_id.id, obj.id,
-                            force_send=True, raise_exception=True,
-                            context=context)
-
-                        # Notificar en el registro que se ha enviado el
-                        # correo
-                        partner_ids = [obj.user_id and obj.user_id.partner_id
-                                       and obj.user_id.partner_id.id] or []
-                        body = _('Mail sent to partner from warning \'%s\'.'
-                                 % self.name)
-                        obj.with_context(
-                            mail_post_autofollow=False).message_post(
-                            body=body, partner_ids=partner_ids)
-
-                        return True
-
+                    # Notificar en el registro que se ha enviado el
+                    # correo
+                    partner_ids = [obj.user_id and obj.user_id.partner_id
+                                   and obj.user_id.partner_id.id] or []
+                    body = _('Mail sent to partner from warning \'%s\'.'
+                             % self.name)
+                    obj.with_context(
+                        mail_post_autofollow=False).message_post(
+                        body=body, partner_ids=partner_ids)
                 else:
                     _log.error('%s model don\'t inherit mail.message, '
                                'not message sended.' % self.model_id.model)
@@ -142,11 +129,25 @@ class WarningMessaging(models.Model):
 class WarningAction(models.Model):
     _inherit = 'warning.action'
 
+    email_tmpl_id = fields.Many2one(
+        comodel_name='email.template',
+        string='Email Template')
+    email_subject = fields.Char(string='Email subject')
+    email_body_html = fields.Char(string='Email body html')
+    email_attachment_ids = fields.Many2many(
+        comodel_name='ir.attachment',
+        relation='warning_mess_attach_rel',
+        column1='warning_mess_id',
+        column2='attachment_id')
+
     # Anadir valores a campo selection
     def __init__(self, pool, cr):
         super(WarningAction, self).__init__(pool, cr)
-        option = ('send_email', 'Send email')
+        options = [
+            ('send_email_with_templ', 'Send email with template'),
+            ('send_email_without_templ', 'Send email without template')]
         type_selection = self._columns['ttype'].selection
 
-        if option not in type_selection:
-            type_selection.append(option)
+        for option in options:
+            if option not in type_selection:
+                type_selection.append(option)
